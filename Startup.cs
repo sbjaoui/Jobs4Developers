410714ce8e6af657bf7651016a2642f726dbc1d3
Said Bjaoui <bejaoui.said@gmail.com>
2015-08-03 13:21:57 -0400

    Initial Commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jobs4Developers.Startup))]
+namespace Jobs4Developers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
